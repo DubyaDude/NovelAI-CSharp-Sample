390ddb557c3f2fffb1f41facd73c8034cc79f4b6
Usman Shafiq <ushafiq141@gmail.com>
2022-10-05 21:14:48 -0400

    Update Program.cs to show off usage

--- NovelAI/Program.cs
+++ NovelAI/Program.cs
@@ -1,18 +1,50 @@
 ﻿using NovelAI;
 using NovelAI.OpenApi;
-using System.Text;
-
-Console.WriteLine("Hello, World!");
 
 string email = "notarealemail@gmail.com";
 string password = "notarealpassword";
 
 NovelClient novelAI = new NovelClient("https://api.novelai.net/", new HttpClient());
-var resp = await novelAI.UserLoginAsync(new LoginRequest() { Key = NovelUtils.GenerateLoginString(email, password) });
+var authResp = await novelAI.UserLoginAsync(new LoginRequest() { Key = NovelUtils.GenerateLoginString(email, password) });
 
-Console.WriteLine("Got AccessToken: " + resp.AccessToken);
+Console.WriteLine("Got AccessToken: " + authResp.AccessToken);
 Console.WriteLine();
 
-var resp2 = await novelAI.UserInformationAsync();
-Console.WriteLine("Got AccountCreatedAt: " + resp2.AccountCreatedAt);
+// Getting current subscription
+var userResp = await novelAI.UserSubscriptionAsync();
+Console.WriteLine("Got Account Tier: " + userResp.Tier);
 
+// Setting up image we want to generate
+var imageParameters = new AiGenerateImageParameters();
+var userInput = "anime girl";
+
+// Getting price of image
+var imagePriceResp = await novelAI.AiGenerateImageRequestPriceAsync(new AiGenerateImagePriceRequest()
+{
+    Tier = (AiGenerateImagePriceRequestTier)userResp.Tier - 1,
+    Request = new Request()
+    {
+        Input = new string[] { userInput },
+        Model = AiGenerateImageModals.NaiDiffusion,
+        Parameters = imageParameters,
+    }
+});
+
+// Rendering only if free
+if (imagePriceResp.RequestEligibleForUnlimitedGeneration || imagePriceResp.CostPerPrompt == 0)
+{
+    Console.WriteLine("Image price free!");
+    var imageResp = await novelAI.AiGenerateImageAsync(new AiGenerateImageRequest()
+    {
+        Input = userInput,
+        Model = AiGenerateImageModals.NaiDiffusion,
+        Parameters = imageParameters,
+    });
+
+    // Format (raw string):
+    //
+    // event: newImage
+    // id: 1
+    // data: ACTUAL-IMAGE-DATA
+    Console.WriteLine("Got Image Resp: \n" + imageResp);
+}
